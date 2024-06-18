6dd498a0f90e60d58313cf7576e96d79717113bc
fnnbrr <9000395+fnnbrr@users.noreply.github.com>
2024-06-18 17:28:25 -0400

    Added a basic footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer>
+        <Footer/>
+      </footer>
     </div>
   );
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,14 @@
+﻿import React from "react";
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <text>made by <a href="https://fnnbrr.com">fnnbrr</a></text>
+            <br/>
+            <text>© 2024</text>
+        </div>
+    );
+}
+
+export default Footer;
