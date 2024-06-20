51dd32bab8f22509ba095513866cc77266e711de
fnnbrr <9000395+fnnbrr@users.noreply.github.com>
2024-06-20 13:04:59 -0400

    Started adding social icons to footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,12 +1,39 @@
 import React from "react";
 import './Footer.css'
 
+interface SocialIconProps {
+    iconUrl: string;
+    redirectUrl: string;
+}
+
+function SocialIcon({iconUrl, redirectUrl}: SocialIconProps) {
+    return (
+        <a href={redirectUrl}>
+            <img src={iconUrl} alt={redirectUrl} className={"SocialButtonImg"}/>
+        </a>
+    )
+}
+
 function Footer() {
     return (
         <div className="Footer">
-            <text>made by <a href="https://fnnbrr.com">fnnbrr</a></text>
-            <br/>
-            <text>© 2024</text>
+            <div>
+                <text>made by <a href="https://fnnbrr.com">fnnbrr</a></text>
+                <br/>
+                <text>© 2024</text>
+            </div>
+            <div className={"SocialButtonContainer"}>
+                <SocialIcon
+                    iconUrl={"logo192.png"}
+                    redirectUrl={"https://google.com"}/>
+                <SocialIcon
+                    iconUrl={"logo192.png"}
+                    redirectUrl={"https://google.com"}/>
+                <SocialIcon
+                    iconUrl={"logo192.png"}
+                    redirectUrl={"https://google.com"}/>
+            </div>
+            
         </div>
     );
 }
