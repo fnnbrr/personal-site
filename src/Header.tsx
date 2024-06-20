4a5eb393295e5a7da96f6dd58663ee3627940baa
fnnbrr <9000395+fnnbrr@users.noreply.github.com>
2024-06-20 16:52:02 -0400

    Added a basic header

---
+++ src/Header.tsx
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import './Header.css'
+
+function Header() {
+    return (
+        <div className={"Header"}>
+            <a href={"https://fnnbrr.com"}>
+                <img src={"fnnbrr_logo_outlined_2x.png"} alt={"https://fnnbrr.com"} className={"Logo"}/>
+            </a>
+            <a href="https://fnnbrr.com">home</a>
+            <a href="https://fnnbrr.com">resume</a>
+            <a href="https://fnnbrr.com">games</a>
+            <a href="https://fnnbrr.com">tools</a>
+            <a href="https://fnnbrr.com">contact</a>
+        </div>
+    );
+}
+
+export default Header;
