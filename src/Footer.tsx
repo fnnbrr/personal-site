fc4a897194b997984733f53af7f3c57eee4d51d4
fnnbrr <9000395+fnnbrr@users.noreply.github.com>
2024-06-20 13:45:13 -0400

    Added social links and sitemap in footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -9,7 +9,7 @@ interface SocialIconProps {
 function SocialIcon({iconUrl, redirectUrl}: SocialIconProps) {
     return (
         <a href={redirectUrl}>
-            <img src={iconUrl} alt={redirectUrl} className={"SocialButtonImg"}/>
+            <img src={iconUrl} alt={redirectUrl} className={"SocialIconImage"}/>
         </a>
     )
 }
@@ -17,23 +17,29 @@ function SocialIcon({iconUrl, redirectUrl}: SocialIconProps) {
 function Footer() {
     return (
         <div className="Footer">
+            <div className={"SiteMapContainer"}>
+                <a href="https://fnnbrr.com">home</a>
+                <text>|</text>
+                <a href="https://fnnbrr.com">wip</a>
+                <text>|</text>
+                <a href="https://fnnbrr.com">wip</a>
+            </div>
             <div>
                 <text>made by <a href="https://fnnbrr.com">fnnbrr</a></text>
                 <br/>
                 <text>© 2024</text>
             </div>
-            <div className={"SocialButtonContainer"}>
+            <div className={"SocialIconContainer"}>
                 <SocialIcon
-                    iconUrl={"logo192.png"}
-                    redirectUrl={"https://google.com"}/>
+                    iconUrl={"social-icons/email-12-128.png"}
+                    redirectUrl={"mailto:fnnbrr.dev@gmail.com"}/>
                 <SocialIcon
-                    iconUrl={"logo192.png"}
-                    redirectUrl={"https://google.com"}/>
+                    iconUrl={"social-icons/In-White-128.png"}
+                    redirectUrl={"https://www.linkedin.com/in/finnbarrocallahan/"}/>
                 <SocialIcon
-                    iconUrl={"logo192.png"}
-                    redirectUrl={"https://google.com"}/>
+                    iconUrl={"social-icons/github-mark-white.svg"}
+                    redirectUrl={"https://github.com/fnnbrr"}/>
             </div>
-            
         </div>
     );
 }
