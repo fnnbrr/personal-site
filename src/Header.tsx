1d20d646289fc41ac739a120147b8d5c76e0b75e
fnnbrr <9000395+fnnbrr@users.noreply.github.com>
2024-06-22 22:42:44 -0400

    Example dropdown menu for hovering header links

--- src/Header.tsx
+++ src/Header.tsx
@@ -9,22 +9,29 @@ interface HeaderLinkProps {
 
 function HeaderLink({route, label}: HeaderLinkProps) {
     return (
-        <NavLink 
-            to={route}
-            className={({isActive}) =>
-                isActive ? "active" : "inactive"
-            }
-        >
-            {label}
-        </NavLink>
+        <div className="link-dropdown">
+            <NavLink
+                to={route}
+                className={({isActive}) =>
+                    isActive ? "header-link active" : "header-link inactive"
+                }
+            >
+                {label}
+            </NavLink>
+            <div className={"dropdown-list"}>
+                <text className={"header-link inactive"}>Link 1</text>
+                <text className={"header-link inactive"}>Link 2</text>
+                <text className={"header-link inactive"}>Link 3</text>
+            </div>
+        </div>
     );
 }
 
 export default function Header() {
     return (
         <div className={"Header"}>
-            <Link to="/">
-                <img src={"fnnbrr_logo_outlined_2x.png"} alt={"https://fnnbrr.com"} className={"Logo"}/>
+            <Link to="/" className={"Logo"}>
+                <img src={"fnnbrr_logo_outlined_2x.png"} alt={"https://fnnbrr.com"}/>
             </Link>
             
             <HeaderLink route={"/"} label={"home"}/>
