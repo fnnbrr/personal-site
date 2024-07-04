1dde16e37d7c672ab2b7845304b94704a1ac7451
fnnbrr <9000395+fnnbrr@users.noreply.github.com>
2024-07-04 14:02:48 -0400

    Added better layout to Scoober Splat page for mobile and widescreen

--- src/routes/Blog1.tsx
+++ src/routes/Blog1.tsx
@@ -1,4 +1,4 @@
-﻿import React from "react";
+import React from "react";
 import './Blogs.css'
 
 export default function Blog1() {
--- src/routes/ScooberSplat.tsx
+++ src/routes/ScooberSplat.tsx
@@ -16,16 +16,7 @@ export default function ScooberSplat() {
             <p>
                 A cross-platform online party game with 70k+ players and a 95% Positive rating on <a href={"https://store.steampowered.com/app/1601830/Scoober_Splat/"} target="_blank" rel={"noreferrer"}>Steam</a>.
             </p>
-            <br/>
-            <div className={"Videos"}>
-                <iframe
-                    src="https://www.youtube.com/embed/rphtcQZmUlA?si=bWw3qZsUZ6nK1jAc"
-                    title="YouTube video player"
-                    className={"Video"}
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerPolicy="strict-origin-when-cross-origin"
-                    allowFullScreen>
-                </iframe>
+            <div className={"grid"}>
                 <iframe
                     src="https://www.youtube.com/embed/6idYouyN1MA?si=HDEByAXBNkl7IuUm"
                     title="YouTube video player"
@@ -34,14 +25,22 @@ export default function ScooberSplat() {
                     referrerPolicy="strict-origin-when-cross-origin"
                     allowFullScreen>
                 </iframe>
+                <p>
+                    "Scoober Splat is a hilarious online party game of monster chase. With up to 16 players, two people play as monstrous Goobers on the hunt. Together, the rest of the Scoobers must repair the submarine in order to escape certain death. After the dust settles, everyone gains a new power-up and it starts all over again, with even more chaos!"
+                </p>
+                <div className={"flex-break"}/>
+                <p>
+                    "With cross-platform play on mobile devices and in your browser, playing with all of your friends is easy as dropping a link in your group chat. Work together and scream along with built-in proximity chat!"
+                </p>
+                <iframe
+                    src="https://www.youtube.com/embed/rphtcQZmUlA?si=bWw3qZsUZ6nK1jAc"
+                    title="YouTube video player"
+                    className={"Video"}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen>
+                </iframe>
             </div>
-            <br/>
-            <p>
-                "Scoober Splat is a hilarious online party game of monster chase. With up to 16 players, two people play as monstrous Goobers on the hunt. Together, the rest of the Scoobers must repair the submarine in order to escape certain death. After the dust settles, everyone gains a new power-up and it starts all over again, with even more chaos!
-            </p>
-            <p>
-                With cross-platform play on mobile devices and in your browser, playing with all of your friends is easy as dropping a link in your group chat. Work together and scream along with built-in proximity chat!"
-            </p>
             <p>
                 Scoober Splat was live from 2021-2022 and is now offline :(
             </p>
